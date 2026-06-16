2e7f3534327e466510fc5ccad465c2788e621690
Win Htut <whks3777@gmail.com>
2026-06-16 16:01:38 +0800

    Refactored tailwind config to include content array

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
   theme: {
     extend: {
       colors: {
@@ -15,5 +21,6 @@ module.exports = {
         'pill': '9999px',
       }
     }
-  }
+  },
+  plugins: [],
 }
