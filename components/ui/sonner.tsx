ff9dd95c20f0fc9fd246d5ba45037201e0536e96
Win Htut <whks3777@gmail.com>
2026-07-10 23:05:02 +0800

    add duration to sonner so users can see read the toast

--- components/ui/sonner.tsx
+++ components/ui/sonner.tsx
@@ -10,6 +10,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
   return (
     <Sonner
       theme={theme as ToasterProps["theme"]}
+      duration={6000}
       className="toaster group"
       icons={{
         success: (
