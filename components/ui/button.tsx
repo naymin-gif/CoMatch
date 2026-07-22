5dd52c7b81a57be162fe950acde8709a609df2f5
Win Htut <whks3777@gmail.com>
2026-07-22 13:50:58 +0800

    Add green and input style variants to button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,6 +19,9 @@ const buttonVariants = cva(
         destructive:
           "bg-destructive/10 text-destructive hover:bg-destructive/20 focus-visible:border-destructive/40 focus-visible:ring-destructive/20 dark:bg-destructive/20 dark:hover:bg-destructive/30 dark:focus-visible:ring-destructive/40",
         link: "text-primary underline-offset-4 hover:underline",
+        input: "flex items-center justify-start hover:bg-muted hover:text-foreground text-muted-foreground border pl-4 gap-2 border-gray-500/30 h-[46px] rounded-full overflow-hidden max-w-md w-full",
+        green: 
+          "bg-green-500/10 text-green-800 hover:bg-green-500/20 focus-visible:border-green-500/40 focus-visible:ring-green-500/20 dark:bg-green-500/20 dark:hover:bg-green-500/30 dark:focus-visible:ring-green-500/40",
       },
       size: {
         default:
