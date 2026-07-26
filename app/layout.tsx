6bad2c23e7c4d45bf96633ded2369a7eb08e6530
Win Htut <whks3777@gmail.com>
2026-07-26 20:39:25 +0800

    Update login page with ui components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 import NavBar from '@/components/layouts/NavBar';
 import { Fredoka } from 'next/font/google';
 import { Toaster } from '@/components/ui/sonner';
+import localFont from 'next/font/local';
 
 const fredoka = Fredoka({
   subsets: ['latin'],
@@ -10,13 +11,23 @@ const fredoka = Fredoka({
   display: 'swap',
 });
 
+const featherBold = localFont({
+  src: '../public/fonts/featherbolds.woff',
+  variable: '--font-feather-bold',
+  weight: '700',
+  style: 'normal',
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={fredoka.variable}>
+    <html
+      lang="en"
+      className={`${fredoka.variable} ${featherBold.variable}`}
+    >
       <body>
         <NavBar />
         {children}
