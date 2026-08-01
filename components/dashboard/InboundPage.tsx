c74f79ef7cc10988c81cc9021232053194dc80af
Win Htut <whks3777@gmail.com>
2026-08-01 22:15:57 +0800

    Add empty page for dashboard so users can clearly know when there is no application to show on the dashboard

--- components/dashboard/InboundPage.tsx
+++ components/dashboard/InboundPage.tsx
@@ -1,12 +1,24 @@
 'use client';
 
 import InboundAppCard, { type InboundAppCardProps } from './InboundAppCard';
+import { TbFileSad } from "react-icons/tb";
 
 export interface InboundPageProps {
   inboundCardProps: InboundAppCardProps[];
 }
 
 export default function InboundPage({ inboundCardProps }: InboundPageProps) {
+  if (inboundCardProps.length === 0) {
+    return (
+      <div className='flex flex-row gap-3 items-center justify-center'>
+        <TbFileSad className='text-xl'/>
+        <span>
+          You do not have any inbound applications to review. <br />
+          Applications will appear here when other users apply your team.
+        </span>
+      </div>
+    );
+  }
   return (
     <div className="flex flex-wrap gap-3">
       {inboundCardProps.map((cardProps, index) => (
--- components/dashboard/OutboundPage.tsx
+++ components/dashboard/OutboundPage.tsx
@@ -1,10 +1,24 @@
 import OutboundAppCard, { type OutboundAppCardProps } from './OutboundAppCard';
+import { TbFileSad } from "react-icons/tb";
 
 interface OutboundPageProps {
   applications: OutboundAppCardProps[];
 }
 
 export default function OutboundPage({ applications }: OutboundPageProps) {
+  
+  if (applications.length === 0) {
+    return (
+      <div className='flex flex-row gap-3 items-center justify-center'>
+        <TbFileSad className='text-xl'/>
+        <span>
+          You do not have any outbound applications. <br />
+          Applications will appear here when you apply to join other teams.
+        </span>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-wrap gap-3">
       {applications.map((cardProps) => (
